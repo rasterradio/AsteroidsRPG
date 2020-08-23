4f2fcbab96e38cab9f1b2c9711cfa346c0c56521
DESKTOP-L0457BT\Ian <idleshub@gmail.com>
2020-08-22 23:14:45 -0400

    Better pathfinding with rigidbody

---
+++ Assets/Scripts/AIBehavior.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class AIBehavior : MonoBehaviour
+{
+    Transform target;
+    public float speed = 200f;
+    public float nextWayPointDistance = 3f;
+
+    bool reachedEndOfPath;
+    int currentWayPoint = 0;
+
+    Path path;
+    Seeker seeker;
+    Rigidbody2D rb;
+
+    private void Start()
+    {
+        seeker = GetComponent<Seeker>();
+        rb = GetComponent<Rigidbody2D>();
+
+        InvokeRepeating("UpdatePath", 0f, 0.5f);
+    }
+
+    private void OnEnable()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    void UpdatePath()
+    {
+        if (seeker.IsDone()) { seeker.StartPath(rb.position, target.position, OnPathComplete); }
+    }
+
+    void OnPathComplete(Path p)
+    {
+        if (!p.error)
+        {
+            path = p;
+            currentWayPoint = 0;
+        }
+    }
+
+    private void Update()
+    {
+        if (path == null) return;
+
+        if (currentWayPoint >= path.vectorPath.Count)
+        {
+            reachedEndOfPath = true;
+            return;
+        }
+        else { reachedEndOfPath = false; }
+
+        Vector2 direction = ((Vector2)path.vectorPath[currentWayPoint] - rb.position).normalized;
+        Vector2 force = direction * speed * Time.deltaTime;
+
+        rb.AddForce(force);
+
+        float distance = Vector2.Distance(rb.position, path.vectorPath[currentWayPoint]);
+
+        if (distance < nextWayPointDistance) { currentWayPoint++; }
+    }
+}
